49bb11ffdf5cc54d76735214df3b13137837567b
sourceTree <sourcetree@mail.com>
2024-01-05 02:10:18 -0700

    23 - Introduction To Streams

--- Hello.java
+++ Hello.java
@@ -10,19 +10,9 @@ class Hello{
             new Person("Charlotte", "Bronte", 45),
             new Person("Mather", "Arnold", 39)
         );
-        System.out.println("using for loop");
-        for(int i = 0; i < persons.size(); i++){
-            System.out.println(persons);
-        }
-        System.out.println("using enhanced for loop");
-        for(Person p:persons){
-            System.out.println(p);
-        }
-        //with method forEach can possibly use thread
-        System.out.println("using method forEach loop");
-        persons.forEach(p -> System.out.println(p));
 
-        System.out.println("using forEach loop writen with method reference");
-        persons.forEach(System.out::println);
+        persons.stream()
+            .filter(p -> p.getLastName().startsWith("C"))
+            .forEach(p -> System.out.println(p.getFirstName()));
     }
 }
