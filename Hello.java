53de19a033cc206e031ff3c63c66686c5486fb0b
sourceTree <sourcetree@mail.com>
2024-01-05 01:40:34 -0700

    method reference takes argument

--- Hello.java
+++ Hello.java
@@ -1,15 +1,29 @@
-public class Hello{    
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+class Hello{
     public static void main(String[] args) {
+        List<Person> persons = Arrays.asList(
+            new Person("Charles", "Dickens", 60),
+            new Person("Lewis", "Carroll", 42),
+            new Person("Thomas", "Carlyle", 51),
+            new Person("Charlotte", "Bronte", 45),
+            new Person("Mather", "Arnold", 39)
+        );       
         //lambda version
-        Thread lmbd = new Thread(() -> printMessage());
-        lmbd.start();
-        
-        //method reference version
-        Thread mRef = new Thread(Hello::printMessage);  //static
-        mRef.start();
+        printConditionaly(persons, p -> true, p -> System.out.println(p));
+
+        //method refference version
+        printConditionaly(persons, p -> true, System.out::println);     //p -> method(p)
     }
 
-    public static void printMessage(){
-        System.out.println("Hello");
+    private static void printConditionaly(List<Person> persons, Predicate<Person> predicate, Consumer<Person> consumer) {
+        for(Person p:persons){
+            if(predicate.test(p)){
+                consumer.accept(p);
+            }
+        }
     }
 }
