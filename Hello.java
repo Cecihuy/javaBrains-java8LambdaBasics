74f8a2c67e24f74404c3ac86b3dd2c1e61bd707c
sourceTree <sourcetree@mail.com>
2024-01-05 01:05:13 -0700

    20 - The this reference in lambdas

--- Hello.java
+++ Hello.java
@@ -1,23 +1,23 @@
 public class Hello{
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-
-        //if you create lambda, no way to assign b=40. it is called closure
-        doProcess(a, new Process() {    
-            @Override
-            public void process(int i) {
-                //b=40;
-                System.out.println(i + b);  //in this case, 'b' is final
-            }            
-        });
-    }
-
-    public static void doProcess(int i, Process p){
+    public void doProcess(int i, Process p){
         p.process(i);
     }
-
-    interface Process{
-        void process(int i);
+    public static void main(String[] args) {
+        Hello thisReferenceExample = new Hello();
+        thisReferenceExample.doProcess(10, new Process() {
+            @Override
+            public void process(int i){
+                System.out.println("value of i is " + i);
+                System.out.println(this);   //'this' keyword is reffer to new Process
+            }
+            @Override
+            public String toString(){
+                return "This is the anonymous inner class";
+            }
+        });
     }
+}
+
+interface Process{
+    void process(int i);
 }
