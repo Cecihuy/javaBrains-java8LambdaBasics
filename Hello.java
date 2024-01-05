a690eb9e3117e2dd542ce357c34892b4dd108b89
sourceTree <sourcetree@mail.com>
2024-01-04 21:09:53 -0700

    13 - Lambda Exercise

--- Hello.java
+++ Hello.java
@@ -1,15 +1,47 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+/*
+ * 1. Sort list by last name
+ * 2. Create a method that prints all elements in the list
+ * 3. Create a method that prints all people that have last name beginning with C
+ */
 class Hello{
     public static void main(String[] args) {
-        Thread myThread = new Thread(new Runnable() {
+        List<Person> persons = Arrays.asList(
+            new Person("Charles", "Dickens", 60),
+            new Person("Lewis", "Carroll", 42),
+            new Person("Thomas", "Carlyle", 51),
+            new Person("Charlotte", "Bronte", 45),
+            new Person("Mather", "Arnold", 39)
+        );
+
+        Collections.sort(persons, new Comparator<Person>() {
             @Override
-            public void run() {
-                System.out.println("printed inside anonymous innerclass");
+            public int compare(Person o1, Person o2) {
+                return o1.getLastName().compareTo(o2.getLastName());
             }            
         });
 
-        myThread.run();
+        printAll(persons);
 
-        Thread myLamdaThread = new Thread(() -> System.out.println("printed inside lambda"));
-        myLamdaThread.run();        
+        System.out.println("spacer");
+        
+        printLastNameBeginWithC(persons);
+    }
+
+    private static void printLastNameBeginWithC(List<Person> persons) {
+        for(Person p:persons){
+            if(p.getLastName().startsWith("C")){
+                System.out.println(p);
+            }
+        }
+    }
+
+    private static void printAll(List<Person> persons) {
+        for(Person p:persons){
+            System.out.println(p);
+        }
     }
 }
--- HelloWorldGreeting.java
+++
@@ -1,6 +0,0 @@
-public class HelloWorldGreeting implements Greeting{
-    @Override
-    public void perform() {
-        System.out.println("Hello world!");
-    }    
-}
