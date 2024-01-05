f647cc609f93892036ed601a1fb82439e284aa1c
sourceTree <sourcetree@mail.com>
2024-01-05 00:05:25 -0700

    17 - Exception Handling in Lambdas

--- Hello.java
+++ Hello.java
@@ -1,37 +1,26 @@
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
-/*
- * 1. Sort list by last name
- * 2. Create a method that prints all elements in the list
- * 3. Create a method that prints all people that have last name beginning with C
- */
+import java.util.function.BiConsumer;
+
 class Hello{
     public static void main(String[] args) {
-        List<Person> persons = Arrays.asList(
-            new Person("Charles", "Dickens", 60),
-            new Person("Lewis", "Carroll", 42),
-            new Person("Thomas", "Carlyle", 51),
-            new Person("Charlotte", "Bronte", 45),
-            new Person("Mather", "Arnold", 39)
-        );
+        int[] someNumbers = {1, 2, 3, 4};
+        int key = 0;
 
-        Collections.sort(persons, (o1, o2) -> o1.getLastName().compareTo(o2.getLastName()));
-
-        performConditionaly(persons, p -> true, p -> System.out.println(p));
-
-        System.out.println("spacer");
-
-        performConditionaly(persons, p -> p.getFirstName().startsWith("C"), p -> System.out.println(p));        
+        process(someNumbers, key, (k, v) -> {
+            try{
+                System.out.println(k / v);
+            } catch(ArithmeticException e){
+                System.out.println("an arithmetic exception happened");
+            }
+        });
     }
 
-    private static void performConditionaly(List<Person> persons, Predicate<Person> predicate, Consumer<Person> consumer) {
-        for(Person p:persons){
-            if(predicate.test(p)){
-                consumer.accept(p);
+    private static void process(int[] someNumbers, int key, BiConsumer<Integer,Integer> biConsumer) {
+        for(int efl:someNumbers){
+            try{
+                biConsumer.accept(efl, key);
+            } catch(ArithmeticException e){
+
             }
         }
-    }
+    }    
 }
